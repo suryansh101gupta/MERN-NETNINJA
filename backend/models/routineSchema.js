3d5d1e4916d617354d4f4c3b19bfd7f146dc1597
suryanshg10 <suryanshg10@gmail.com>
2025-06-13 17:23:24 +0530

    initiate db lesson 4

---
+++ backend/models/routineSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const routineSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    product: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Number,
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Routine', routineSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const routineRoutes = require('./routes/routines')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
 // routes
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+app.use('/api/routines', routineRoutes)
+
+// connet to db
+mongoose.connect(process.env.URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+    console.log('connected to db and listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
 // listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
