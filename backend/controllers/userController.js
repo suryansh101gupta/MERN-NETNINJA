fd2560a085b99dbefff89cec375df0247e4760ad
suryanshg10 <suryanshg10@gmail.com>
2025-06-17 09:25:25 +0530

    add login and signup logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,42 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (id) => {
+    return jwt.sign({_id:id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 //login user
 const loginUser = async(req, res) => {
-    res.json({msssg: 'login user'})
+    const {email, password} = req.body
+    try{
+        const user = await User.login(email, password)
+
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email,token})
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 // signup user
 const signupUser = async(req, res) => {
-    res.json({msssg: 'signup user'})
+
+    const {email, password} = req.body
+
+    try{
+        const user = await User.signup(email, password)
+
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email,token})
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+
+    // res.json({msssg: 'signup user'})
 }
 
 module.exports = {
