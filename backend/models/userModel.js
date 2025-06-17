52211fdabf2d676080b89e0407222d9bcc653c59
suryanshg10 <suryanshg10@gmail.com>
2025-06-17 09:25:25 +0530

    add login and signup logic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -14,5 +16,59 @@ const userSchema = new Schema({
         required: true,
     }
 })
+
+// statis signup method
+userSchema.statics.signup = async function(email,password) {
+
+    // validation
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email not valid')
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if(exists){
+        throw Error('Email in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({email, password: hash})
+
+    return user
+}
+
+// statics login method
+userSchema.statics.login = async function(email, password) {
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)){
+        throw Error('Email not valid')
+    }
+    const user = await this.findOne({ email })
+
+    if(!user){
+        throw Error('Incorrect Email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect Password')
+    }
+
+    return user
+}
+
+
+
 
 module.exports = mongoose.model('User', userSchema)
